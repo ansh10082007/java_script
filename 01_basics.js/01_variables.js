593ea79a08c30099378a072787baefbf94d1d8ae
ansh10082007 <anshbomble37@gmail.com>
2025-12-19 14:30:29 +0000

    Initial commit

---
+++ 01_basics.js/01_variables.js
@@ -0,0 +1,15 @@
+// this is comment 
+/*
+this is also a comment
+*/
+
+const accountId=6769
+let email="anshbomble37091@gmail.com"
+var city="mumbai"               //dont use this method for variable instead use let method
+let state;    
+
+// accountId=98    //this will give errror
+city="Banglore"
+
+console.log(accountId);
+console.table([accountId,email,city,state])
