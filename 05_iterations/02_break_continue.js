e30bfce3dc6cb38c54ac039cf3ef6ee624aecb2b
ansh10082007 <anshbomble37@gmail.com>
2026-01-01 14:42:41 +0000

    updated

---
+++ 05_iterations/02_break_continue.js
@@ -0,0 +1,23 @@
+// break and continue
+
+/*
+for(let index =1; index<=20; index++){
+    if(index==5){
+        console.log(`Detected 5`);
+        break;      //it stops the whole loop if it is executed
+    }
+    console.log(`Value of i is ${index}`);
+}
+*/
+
+
+/*
+for(let index =1; index<=20; index++){
+    if(index==5){
+        console.log(`Detected 5`);
+        continue;    
+        //it will continue the loop,understand the concept by checking output  
+    }
+    console.log(`Value of i is ${index}`);
+}
+*/
