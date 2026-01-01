4c0665ed5af4e411a7f4bcc94a54c154ff9ad3b2
ansh10082007 <anshbomble37@gmail.com>
2026-01-01 10:04:14 +0000

    updated

---
+++ 04_control_flow/02_switch.js
@@ -0,0 +1,59 @@
+/*
+SWITCH SYNTAX==>
+    
+switch(key){
+    case value:
+
+        break;
+    .
+    .
+    .
+    default:
+        .
+        break;
+}
+*/
+
+/*
+const month = 3
+switch(month){
+    case 1:
+        console.log("jan");
+        break;
+    case 2:
+        console.log("Feb");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+    default:
+        console.log("Default case match");
+        break;
+}
+*/
+
+/*
+const month = "feb"
+switch(month){
+    case "jan":
+        console.log("jan");
+        break;
+    case "feb":
+        console.log("Feb");
+        break;
+    case "march":
+        console.log("March");
+        break;
+    case "april":
+        console.log("April");
+        break;
+    default:
+        console.log("Default case match");
+        break;
+}
+*/
+
+
---
+++ 04_control_flow/1_if_else.js
@@ -0,0 +1,61 @@
+/*
+const isUserloggedIn=true
+
+if(isUserloggedIn){
+    console.log("executed1");//will be executed
+}
+
+if(3!=2){
+    console.log("executed2");//will be executed
+}
+
+if(2=="2"){
+    console.log("executed3");//will be executed
+}
+
+if(2==="2"){//checks strictly ,checks value and datatype also
+    console.log("executed4");//will not be executed
+}
+*/
+
+
+
+/*
+const score = 200
+if(score>200){
+    const power="fly"
+    console.log(`User power: ${power}`);
+}
+
+console.log(`User power':${power}`);  //this will give error cause of scope concept
+*/
+
+
+/*
+const balance = 1000
+if(balance<500){
+    console.log("less than 500");
+}
+else if(balance < 750){
+    console.log("less than 750");
+}
+else {
+    console.log("less than 1500");
+}
+*/
+
+
+/*
+const UserloggedIn=true
+const debitcard=true
+const loggedInfromGoogle=false
+const loggedInfromEmail=true
+
+if(UserloggedIn && debitcard && 2==3){
+    console.log("allow user!");
+}
+
+if(loggedInfromEmail || loggedInfromGoogle){
+    console.log("Allow");
+}
+*/
