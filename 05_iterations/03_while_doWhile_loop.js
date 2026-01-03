f98227ca58f92575c620e6d772f38f6b067c4797
ansh10082007 <anshbomble37@gmail.com>
2026-01-03 05:23:27 +0000

    updated

---
+++ 05_iterations/03_while_doWhile_loop.js
@@ -0,0 +1,34 @@
+/*
+while(condition){
+
+}
+*/
+
+
+/*
+let index=0
+while(index<=10){
+    console.log(`value of index is ${index}`);
+    index=index+2;
+}
+*/
+
+
+/*
+let myArr=['flash','batman',"superman"]
+let arr=0;
+while(arr<myArr.length){
+    console.log(`Value is ${myArr[arr]}`);
+    arr++;
+}
+*/
+
+/*
+let score = 1
+do{
+    console.log(`Score is ${score}`)
+    score++;
+}while(score<=10);
+*/
+
+
