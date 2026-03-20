213151648d449aab161ecf18eb54737abcf71cde
ansh10082007 <anshbomble37@gmail.com>
2026-03-20 15:34:22 +0000

    updated

--- 11_advance_one/promises.js
+++ 11_advance_one/promises.js
@@ -1,83 +1,83 @@
-// const promiseOne = new Promise(function(resolve,reject){
-//     //Do an async task 
-//     setTimeout(function(){
-//         console.log('Async task is complete');
-//         resolve()
-//     },1000)
-// })
-// promiseOne.then(function(){
-//     console.log("Promise consumed");
-// })
+const promiseOne = new Promise(function(resolve,reject){
+    //Do an async task 
+    setTimeout(function(){
+        console.log('Async task is complete');
+        resolve()
+    },1000)
+})
+promiseOne.then(function(){
+    console.log("Promise consumed");
+})
 
 
-// new Promise(function(resolve,reject){
-//     setTimeout(function(){
-//         console.log("Async Task 2");
-//         resolve();
-//     },1000)
-// }).then(function(){
-//     console.log("Aysnc task 2 resolve");  
-// })
+new Promise(function(resolve,reject){
+    setTimeout(function(){
+        console.log("Async Task 2");
+        resolve();
+    },1000)
+}).then(function(){
+    console.log("Aysnc task 2 resolve");  
+})
 
 
-// const promiseThree = new Promise(function(resolve,reject){
-//     setTimeout(function(){
-//         resolve({username:"chai",email:"chaiexample@gmail.com"})
-//     },1000)
-// })
-// promiseThree.then(function(user){ 
-//     console.log(user);
-// })
+const promiseThree = new Promise(function(resolve,reject){
+    setTimeout(function(){
+        resolve({username:"chai",email:"chaiexample@gmail.com"})
+    },1000)
+})
+promiseThree.then(function(user){ 
+    console.log(user);
+})
 
-// const promiseFour = new Promise(function(resolve,reject){
-//     setTimeout(function(){
-//         let error = false   //type here true also so u can see error also !!
-//         if(!error){
-//             resolve({username:"ansh",password:"12345"})
-//         }else{
-//             reject('ERROR something went wrong')
-//         }
-//     },1000)
-// })
+const promiseFour = new Promise(function(resolve,reject){
+    setTimeout(function(){
+        let error = false   //type here true also so u can see error also !!
+        if(!error){
+            resolve({username:"ansh",password:"12345"})
+        }else{
+            reject('ERROR something went wrong')
+        }
+    },1000)
+})
 
-// promiseFour
-// .then((user)=>{
-//     console.log(user);
-//     return user.username;
-// })
-// .then((myusername)=>{
-//     console.log(myusername);
-// })
-// .catch(function(error){
-//     console.log(error);
-// })
-// .finally(()=>{
-//     console.log("The promise is resolved or rejected");
-// })
+promiseFour
+.then((user)=>{
+    console.log(user);
+    return user.username;
+})
+.then((myusername)=>{
+    console.log(myusername);
+})
+.catch(function(error){
+    console.log(error);
+})
+.finally(()=>{
+    console.log("The promise is resolved or rejected");
+})
 
 
-// const promisefive = new Promise(function(resolve,reject){
-//     setTimeout(function(){
-//         let error = true ;
-//         if(!error){
-//             resolve({username:"Javascript",password:"123"})
-//         }
-//         else{
-//             reject("Error");
-//         }
-//     },1000)
-// })
+const promisefive = new Promise(function(resolve,reject){
+    setTimeout(function(){
+        let error = true ;
+        if(!error){
+            resolve({username:"Javascript",password:"123"})
+        }
+        else{
+            reject("Error");
+        }
+    },1000)
+})
 
-// async function consumePromiseFive(){
-//     try{    
-//         const response = await promisefive;
-//         console.log(response);
-//     }
-//     catch(error){
-//         console.log(error);
-//     }
-// }
-// consumePromiseFive()
+async function consumePromiseFive(){
+    try{    
+        const response = await promisefive;
+        console.log(response);
+    }
+    catch(error){
+        console.log(error);
+    }
+}
+consumePromiseFive()
 
 async function getAllUsers(){
     try{
